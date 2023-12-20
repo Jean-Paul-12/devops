9e7194bdaee7816f3433e8af65874fa3f24ae416
Jean Paul Quitian <j.guillen.0612@gmail.com>
2023-12-20 09:08:10 -0500

    Update Sonarqbe

--- src/lb_analisissonarqube.groovy
+++ src/lb_analisissonarqube.groovy
@@ -4,8 +4,8 @@ def mysonarScan() {
                     sh "${scannerHome}/bin/sonar-scanner \
                            -Dsonar.projectKey=crudSpringBoot \
                            -Dsonar.projectName=crudSpringBoot \
-                           -Dsonar.sources=src/main/java \
-                           -Dsonar.java.binaries=target/classes \
+                           -Dsonar.sources=. \
+                           -Dsonar.java.binaries=. \
                            -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml"
     echo "Escaneo SonarQube Verificado"
                 }
